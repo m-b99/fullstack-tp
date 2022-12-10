a81d7c48a0460b80f45233f161060136b8df87a9
mohamed-baiba <mohamed_baiba@um5.ac.ma>
2022-12-10 13:48:22 +0100

    done TP3

--- spring-backend/src/main/java/ma/ensias/springbackend/service/ModuleService.java
+++ spring-backend/src/main/java/ma/ensias/springbackend/service/ModuleService.java
@@ -1,56 +1,42 @@
 package ma.ensias.springbackend.service;
 
 
+import lombok.AllArgsConstructor;
 import ma.ensias.springbackend.module.Module;
+import ma.ensias.springbackend.repository.ModuleRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @Service
+@AllArgsConstructor
 public class ModuleService {
-
-    private List<Module> modules = new ArrayList<>( Arrays.asList( new Module(1L, "java Standard Edition Platform" , "javaSE"),
-            new Module(2L , "java Enterprise Edition Platform" , "Java EE") ) ) ;
-
+    private ModuleRepository moduleRepository ;
 
     public List<Module> getAllModules() {
-        return
-                modules
-                ;
+
+        List<Module> modules = new ArrayList<>() ;
+        moduleRepository.findAll().forEach(modules::add);
+
+        return modules ;
     }
 
 
     public Module getModule(Long id) {
-        for ( Module module : modules  ) {
-            if ( id.equals(module.getId()) ) {
-                return module ;
-            }
-        }
-
-        return null ;
+        return moduleRepository.findById(id).orElse(null) ;
     }
 
     public Module addModule( Module module ) {
-        if ( module == null ) return null ;
-        modules.add(module) ;
-
-        return module ;
+        return moduleRepository.save(module) ;
     }
 
     public void modifierModule(Long id, Module module) {
-        for (int i=0; i<modules.size(); i++) {
-            Module m = modules.get(i);
-            if (id.equals(m.getId())) {
-                modules.set(i, module);
-                return;
-            }
-        }
+        moduleRepository.save(module) ;
     }
 
     public void supprimerModule(Long id) {
-        modules.removeIf(m -> id.equals(m.getId()));
+        moduleRepository.delete(moduleRepository.findById(id).orElse(null));
     }
 
 
