d092b28a8ab4bd458b30011853e3385395f9e009
mohamed-baiba <mohamed_baiba@um5.ac.ma>
2022-12-10 15:26:02 +0100

    done TP4

--- spring-backend/src/main/java/ma/ensias/springbackend/SpringBackendApplication.java
+++ spring-backend/src/main/java/ma/ensias/springbackend/SpringBackendApplication.java
@@ -2,8 +2,13 @@ package ma.ensias.springbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableWebMvc
+@EnableSwagger2
 public class SpringBackendApplication {
 
     public static void main(String[] args) {
--- spring-backend/src/main/java/ma/ensias/springbackend/module/Cours.java
+++ spring-backend/src/main/java/ma/ensias/springbackend/module/Cours.java
@@ -4,17 +4,17 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+
 
 @Entity
 @Data @AllArgsConstructor @NoArgsConstructor
-public class Module {
+public class Cours {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY )
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id ;
     private String nom ;
     private String description ;
+    @ManyToOne
+    private Module module ;
 }
