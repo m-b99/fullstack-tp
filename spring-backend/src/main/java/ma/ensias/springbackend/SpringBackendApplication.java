cd67f62211f4b56d49a8ebd4226b2d082ba0e7d1
mohamed-baiba <mohamed_baiba@um5.ac.ma>
2022-12-13 23:18:41 +0100

    add some data

--- spring-backend/src/main/java/ma/ensias/springbackend/SpringBackendApplication.java
+++ spring-backend/src/main/java/ma/ensias/springbackend/SpringBackendApplication.java
@@ -1,8 +1,11 @@
 package ma.ensias.springbackend;
 
+import ma.ensias.springbackend.module.Module;
+import ma.ensias.springbackend.service.ModuleService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -15,4 +18,13 @@ public class SpringBackendApplication {
         SpringApplication.run(SpringBackendApplication.class, args);
     }
 
+
+    @Bean
+    CommandLineRunner commandLineRunner(ModuleService moduleService) {
+        return args -> {
+            moduleService.addModule( new Module(null , "JSP" , "Java Server Page")) ;
+            moduleService.addModule( new Module(null , "JEE" , "Java Enterprise Edition")) ;
+            moduleService.addModule( new Module(null , "JSE" , "Java Standard Edition")) ;
+        } ;
+    }
 }
